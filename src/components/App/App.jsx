c161de34455d79173c54e6fb0b4cedd0a1ba2b75
Michael Chuinard <mpchuinard@finlandia.edu>
2022-04-01 17:19:19 -0500

    setup routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,64 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useState, useEffect } from 'react';
+import FeedbackForm from '../FeedbackForm/FeedbackForm';
+import FeelingForm from '../FeelingForm/FeelingForm';
+import ReviewForm from '../ReviewForm/ReviewForm';
+import SupportForm from '../SupportForm/SupportForm';
+import ThanksForm from '../ThanksForm/ThanksForm';
+import UnderstandingForm from '../UnderstandingForm/UnderstandingForm';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
 
 function App() {
-
+  
   return (
+    <Router>
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      
+          <ul>
+            <li>
+              <Link to='/feeling'>
+                {/* <FeelingForm /> */}
+                </Link>
+            </li>
+            <li>
+              <Link to='/understanding'>
+                {/* <UnderstandingForm /> */}
+                </Link>
+            </li>
+            <li>
+              <Link to='/support'>
+                {/* <SupportForm /> */}
+                </Link>
+            </li>
+            <li>
+              <Link to='/comments'>
+                {/* <FeedbackForm /> */}
+                </Link>
+            </li>
+            <li>
+              <Link to='/review'>
+                {/* <ReviewForm /> */}
+                </Link>
+            </li>
+            <li>
+              <Link to='/thanks'>
+                {/* <ThanksForm /> */}
+                </Link>
+            </li>
+          </ul>
+        
     </div>
+    
+    </Router>
   );
 }
 
---
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+function FeelingForm () {
+    console.log('In feeling form!')
+    return (
+    <h2>How are you feeling today?</h2>
+
+        )
+}
+
+export default FeelingForm;
