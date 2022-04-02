932be43fd448bb000bf2a2e708b943149bc2174c
Michael Chuinard <mpchuinard@finlandia.edu>
2022-04-02 14:22:33 -0500

    add first reducer to index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+const feelingReducer = (state = [], action) => {
+    if (action.type === 'ADD_FEELING') {
+        return action.payload;
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        feelingReducer
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
